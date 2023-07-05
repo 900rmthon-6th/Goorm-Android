a741c6e365f52d06ac9e29ccc378938e84ebd943
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2023-07-06 02:46:30 +0900

    [Setting] 초기 세팅
    - MainActivity BindingActivity 적용

--- app/src/main/java/com/sunul/sunul/presentation/MainActivity.kt
+++ app/src/main/java/com/sunul/sunul/presentation/MainActivity.kt
@@ -3,12 +3,13 @@ package com.sunul.sunul.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sunul.sunul.R
+import com.sunul.sunul.binding.BindingActivity
+import com.sunul.sunul.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
