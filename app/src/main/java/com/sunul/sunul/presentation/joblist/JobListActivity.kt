39a9a52435a7544e7b2a064c5149baecec54b3c2
jemma <ejayjeon@gmail.com>
2023-07-06 23:57:47 +0900

    [Ui] result_activity -> job_list_activity 추가

--- app/src/main/java/com/sunul/sunul/presentation/joblist/JobListActivity.kt
+++ app/src/main/java/com/sunul/sunul/presentation/joblist/JobListActivity.kt
@@ -1,23 +1,21 @@
-package com.sunul.sunul.presentation.intro
+package com.sunul.sunul.presentation.joblist
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import com.sunul.sunul.R
 import com.sunul.sunul.binding.BindingActivity
-import com.sunul.sunul.databinding.ActivityIntroBinding
-import com.sunul.sunul.databinding.ActivityMainBinding
-import com.sunul.sunul.presentation.MainActivity
+import com.sunul.sunul.databinding.ActivityJobListBinding
+import com.sunul.sunul.presentation.intro.IntroActivity
 import com.sunul.sunul.presentation.prologue.PrologueActivity
 
-class IntroActivity : BindingActivity<ActivityIntroBinding>(R.layout.activity_intro) {
-
+class JobListActivity : BindingActivity<ActivityJobListBinding>(R.layout.activity_job_list) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 검사 다시하기: IntroActivity로 이동, User 정보 null 필요
         binding.testBtn.setOnClickListener {
-            val intent = Intent(this, PrologueActivity::class.java)
+            val intent = Intent(this, IntroActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
--- app/src/main/java/com/sunul/sunul/presentation/result/ResultActivity.kt
+++ app/src/main/java/com/sunul/sunul/presentation/result/ResultActivity.kt
@@ -1,19 +1,20 @@
-package com.sunul.sunul.presentation.prologue
+package com.sunul.sunul.presentation.result
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sunul.sunul.R
 import com.sunul.sunul.binding.BindingActivity
-import com.sunul.sunul.databinding.ActivityPrologueBinding
-import com.sunul.sunul.presentation.MainActivity
+import com.sunul.sunul.databinding.ActivityResultBinding
+import com.sunul.sunul.presentation.joblist.JobListActivity
+import com.sunul.sunul.presentation.prologue.PrologueActivity
 
-class PrologueActivity : BindingActivity<ActivityPrologueBinding>(R.layout.activity_prologue) {
+class ResultActivity : BindingActivity<ActivityResultBinding>(R.layout.activity_result) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.testBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, JobListActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
