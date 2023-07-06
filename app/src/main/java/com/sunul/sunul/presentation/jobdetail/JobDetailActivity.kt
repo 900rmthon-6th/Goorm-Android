e61de453e5c2803d5489cad0992e97fec51ae7dc
jemma <ejayjeon@gmail.com>
2023-07-07 03:24:42 +0900

    [Ui] job_detail_activity xml 작성

---
+++ app/src/main/java/com/sunul/sunul/presentation/jobdetail/JobDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.sunul.sunul.presentation.jobdetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sunul.sunul.R
+import com.sunul.sunul.binding.BindingActivity
+import com.sunul.sunul.databinding.ActivityJobDetailBinding
+
+class JobDetailActivity : BindingActivity<ActivityJobDetailBinding>(R.layout.activity_job_detail) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/sunul/sunul/presentation/joblist/JobListActivity.kt
+++ app/src/main/java/com/sunul/sunul/presentation/joblist/JobListActivity.kt
@@ -7,6 +7,7 @@ import com.sunul.sunul.R
 import com.sunul.sunul.binding.BindingActivity
 import com.sunul.sunul.databinding.ActivityJobListBinding
 import com.sunul.sunul.presentation.intro.IntroActivity
+import com.sunul.sunul.presentation.jobdetail.JobDetailActivity
 import com.sunul.sunul.presentation.prologue.PrologueActivity
 
 class JobListActivity : BindingActivity<ActivityJobListBinding>(R.layout.activity_job_list) {
@@ -16,6 +17,13 @@ class JobListActivity : BindingActivity<ActivityJobListBinding>(R.layout.activit
         // 검사 다시하기: IntroActivity로 이동, User 정보 null 필요
         binding.testBtn.setOnClickListener {
             val intent = Intent(this, IntroActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.cardView.setOnClickListener {
+            val intent = Intent(this, JobDetailActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
