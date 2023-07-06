4d89258e099cc50e06e77c20a5ab3ffbf5a1d576
jemma <ejayjeon@gmail.com>
2023-07-06 21:47:10 +0900

    [Ui] intro_activity -> prologue_activity 추가

---
+++ app/src/main/java/com/sunul/sunul/presentation/prologue/PrologueActivity.kt
@@ -0,0 +1,12 @@
+package com.sunul.sunul.presentation.prologue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sunul.sunul.R
+
+class PrologueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_prologue)
+    }
+}
